68c05d43c7495e6ccc10d51e73a593c750202345
LVeraPizza <veinticinco95@hotmail.com>
2024-12-06 14:38:22 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -79,7 +79,7 @@ function getProductPrice(productName) {
         "1 Pizza Margherita + 1 Pizza Romana Rúcula y JC": 15000,
         "1 Rucula y jC + 1 Pepperoni": 14000, //promocion 1
         "1 Margherita + 1 4 Formaggi": 13000, // promocion 2
-        "2 Pizzas Margheritas": 14000, // promocion puntos
+        "2 Pizzas Margheritas": 12000, // promocion puntos
         "Pizza Romana Caprese": 10000,
         "Pizza Romana 4Formaggi": 10000,
         "Pizza Romana Rúcula y Jamón Crudo": 10000,
